2a61339974e255d53ad1425e2fddfa8d0fd1fb0a
gloomysw <sw.gloomysw@gmail.com>
2016-07-18 18:12:07 +0800

    1.说明文档更新 2.完善日志读取类

--- fileRead.go
+++ fileRead.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"github.com/smtc/glog"
 	"io"
 	"os"
-	"github.com/smtc/glog"
 	"strings"
 )
 
@@ -14,59 +14,68 @@ import (
 创建时间:2016年6月1日09:40:03
 输入参数:filePath 文件地址
 输出参数:文件对象 错误对象
- */
-func openFile(filePath string) (*os.File,error) {
+*/
+func openFile(filePath string) (*os.File, error) {
 	var (
-		fs *os.File
+		fs  *os.File
 		err error
 	)
-	fs,err=os.Open(filePath)
+	fs, err = os.Open(filePath)
 	if err != nil {
-		glog.Error("open file is error! filePath: %s err: %s \n",filePath,err.Error())
-		return nil,err
+		glog.Error("open file is error! filePath: %s err: %s \n", filePath, err.Error())
+		return nil, err
 	}
-	glog.Info("file open success! filePath: %s \n",filePath)
-	return fs,nil
+	glog.Info("file open success! filePath: %s \n", filePath)
+	return fs, nil
 }
 
 /**
 逐行读文件
 创建人:邵炜
 创建时间:2016年6月1日09:49:45
-输入参数:文件地址 赛选条件方法 回调方法
- */
-func readFile(filepath string,where func(string) bool,callBack func(string)) {
+输入参数:文件地址 赛选条件方法(传入匹配字符串) 回调方法(传入匹配字符串)
+*/
+func readFile(filepath string, where []func(string) bool, callBack []func(string)) (int, int) {
 	var (
-		readAll =false
-		readByte []byte
-		line []byte
-		err error
+		readAll     = false
+		readByte    []byte
+		line        []byte
+		err         error
 		contentLine string
+		matchNumber = 0
+		count       = 0
 	)
-	read,err:=openFile(filepath)
+	read, err := openFile(filepath)
 	if err != nil {
-		return
+		return 0, 0
 	}
 	defer read.Close()
-	buf:=bufio.NewReader(read)
-	for err!=io.EOF {
-		if err!=nil {
-			glog.Error("read error! err: %s \n",err.Error())
+	buf := bufio.NewReader(read)
+	for err != io.EOF {
+		if err != nil {
+			glog.Error("read error! err: %s \n", err.Error())
 		}
 		if readAll {
-			readByte,readAll,err=buf.ReadLine()
-			line=append(line,readByte...)
-		}else{
-			readByte,readAll,err=buf.ReadLine()
-			line=append(line,readByte...)
-			if len(strings.TrimSpace(string(line)))==0 {
+			readByte, readAll, err = buf.ReadLine()
+			line = append(line, readByte...)
+		} else {
+			readByte, readAll, err = buf.ReadLine()
+			line = append(line, readByte...)
+			if len(strings.TrimSpace(string(line))) == 0 {
 				continue
 			}
-			contentLine=string(line)
-			if where(contentLine) {
-				callBack(contentLine)
+			contentLine = string(line)
+			for index, value := range where {
+				if value(contentLine) {
+					if len(callBack) == (index+1) && callBack[index] != nil {
+						callBack[index](contentLine)
+					}
+					matchNumber++
+				}
 			}
-			line=line[:0]
+			line = line[:0]
+			count++
 		}
 	}
+	return matchNumber, count
 }
